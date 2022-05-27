13aef364f902eb5ac72135885b15161f17037a7a
yunnix22 <aboomarmoshab22@gmail.com>
2022-05-28 05:50:10 +0700

    refactor: refactor long form `if` `else` clause

--- index.js
+++ index.js
@@ -53,22 +53,16 @@ function lakukanLooping(arrPegawai) {
     
     hasilLooping.push(`${namaDepan} ${namaBelakang}`);
     
-    if (jenisKelamin === 'F') {
-      jumlahWanita++;
-    } else {
-      jumlahPria++;
-    }
+    jenisKelamin === 'F'? jumlahWanita++ : jumlahPria++
   }
 
   let komentar = '';
 
-  if (jumlahPria > jumlahWanita) {
-    komentar = 'Jumlah Pria lebih banyak dari Wanita';
-  } else if (jumlahWanita > jumlahPria) {
-    komentar = 'Jumlah Wanita lebih banyak dari Pria';
-  } else {
-    komentar = 'Jumlah Pria dan Wanita berimbang';
-  }
+  if (jumlahPria > jumlahWanita) { komentar = 'Jumlah Pria lebih banyak dari Wanita'; }
+ 
+  if (jumlahPria < jumlahWanita) { komentar = 'Jumlah Wanita lebih banyak dari Pria'; }
+
+  if (jumlahPria === jumlahWanita) { komentar = 'Jumlah Pria dan Wanita berimbang'; }
 
   // ! JANGAN DIMODIFIKASI
   return {
